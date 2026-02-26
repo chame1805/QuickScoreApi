919eed5bde4e2575b076d39b58438f004a484550
Angel Chame <angelchame6@gmail.com>
2026-02-25 19:33:05 -0600

    se compuso error de cors

--- docs/docs.go
+++ docs/docs.go
@@ -602,7 +602,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8090",
+	Host:             "quickscoreapi.duckdns.org:8090",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "QuickScore API",
